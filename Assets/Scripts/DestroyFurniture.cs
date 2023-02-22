6814779cb069c4d01dbc76fc824daf66dc95d626
Umair Khalid <52261465+UmairKhalid98@users.noreply.github.com>
2023-02-21 20:04:13 -0700

    closest enemy goes to the destroyed object

---
+++ Assets/Scripts/DestroyFurniture.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFurniture : MonoBehaviour
+{
+    Ray ray;
+    Camera main_camera;
+    RaycastHit hit;
+    GameObject[] enemies;
+
+    void Start()
+    {
+        main_camera = Camera.main;
+        ray = new Ray();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+    void Update()
+    {
+        ray = main_camera.ScreenPointToRay(Input.mousePosition);
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Furniture"))
+            {
+                AlertEnemy(hit.transform.gameObject);
+            }
+
+        }
+    }
+
+    void AlertEnemy(GameObject furniture)
+    {
+        //there is always going to be atleast one enemy around
+        GameObject closestEnemy = enemies[0];
+        float closestDistance = Mathf.Infinity;
+        foreach (GameObject enemy in enemies)
+        {
+            float currDistance = Vector3.Distance(enemy.transform.position, furniture.transform.position);
+            if ( currDistance <= closestDistance)
+            {
+                Debug.Log("Enemy Name " + enemy.name + " curr distance " + currDistance + "closestDistance " + closestDistance );
+                closestEnemy = enemy;
+                closestDistance = currDistance;
+            }
+        }
+        closestEnemy.GetComponent<EnemyController>().InspectFurniture(furniture.transform);
+        Destroy(furniture);
+    }
+}
