1ce0479f5494142405e7eda5bcb0e534cdeac902
laothomas01 <53920510+laothomas01@users.noreply.github.com>
2023-02-27 13:03:15 -0800

    fixed game over UI and pause UI functionality

--- Assets/Scripts/ManageGameState.cs
+++ Assets/Scripts/ManageGameState.cs
@@ -5,48 +5,23 @@ using UnityEngine.SceneManagement;
 public class ManageGameState : MonoBehaviour
 {
     public GameObject pauseMenu = null;
-    public GameObject gameOverScreen = null;
-    public GameObject score = null;
     bool visibleCursor;
     
     bool isPaused;
-    bool gameOver;
 
-    float timeBeforeWinning;
-    float maxTimeBeforeWin;
     void Start()
     {
-        timeBeforeWinning = 0;
-        maxTimeBeforeWin = 4;
-        ;
-        Time.timeScale = 1;
 
         visibleCursor = false;
-        Cursor.visible = visibleCursor;
         isPaused = false;
-        gameOver = false;
     }
     private void Update() {
-  
-  
+
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-           togglePause();
+            togglePause();
         }
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            ResetGame();
-        }
-        
-        // if(score.GetComponent<Score>().getCurrCount() == 0)
-        // {
-        //             timeBeforeWinning += Time.deltaTime;
-        //             if(timeBeforeWinning > maxTimeBeforeWin)
-        //             {
-        //                 winGame();
-        //             }
-        // }
-        Cursor.visible = visibleCursor;
     }    
 
     public bool getIsPaused()
@@ -55,36 +30,17 @@ public class ManageGameState : MonoBehaviour
     }
     public void togglePause()
     {
-               Cursor.visible = visibleCursor;
+           
             isPaused = !isPaused;
             visibleCursor = !visibleCursor;
+              Cursor.visible = visibleCursor;
             Time.timeScale = isPaused ? 0 : 1;       
             pauseMenu.SetActive(isPaused);
             
     }
-    public void toggleGameOverScreen()
-    {
-               isPaused = !isPaused;
-            visibleCursor = !visibleCursor;
-             Cursor.visible = visibleCursor;
 
-            Time.timeScale = isPaused ? 0 : 1;  
-            gameOverScreen.SetActive(isPaused);
-    }
-    public void toggleRetry()
-    {
-       ResetGame();
-    }
-    public void ResetGame()
-    {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
-    
-    public void winGame()
-    {
-             SceneManager.LoadScene("Win_Screen");
-
-    }
+ 
+  
     public void backToMainMenu()
     {
         SceneManager.LoadScene("Main_Menu");
