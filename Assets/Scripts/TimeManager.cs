6e24bc47e5814eda1c692f7b04e0525f58794833
Umair Khalid <52261465+UmairKhalid98@users.noreply.github.com>
2023-03-01 11:16:16 -0700

    Added timer, bug fixes

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+    private float time;
+    private TimeSpan timespan;
+    private string timeElapsed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = this.GetComponent<TextMeshProUGUI>();
+        time = 0;
+        textMesh.SetText("Test");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timespan = TimeSpan.FromSeconds(time);
+        timeElapsed = String.Format(@"{0:mm\:ss\.ff}", timespan);
+        textMesh.SetText(timeElapsed);
+        //+ ":" + TimeSpan.FromSeconds(time) + ":" + TimeSpan.FromMilliseconds(time)
+    }
+}
